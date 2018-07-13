c680e3413dfd93f3be5e280557dfa2c29fc647fc
gy162134 <gy162134@gmail.com>
2018-07-13 16:01:22 -0500

    first problem solved

--- assessment.js
+++ assessment.js
@@ -1,7 +1,7 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
-
+var hello = 'goodbye';
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
